6163f9e936676187b6b035129f30d0ffefb54bce
Roger Ngo <urbanspr1nter@gmail.com>
2018-03-09 20:31:31 -0800

    Color selection and refactoring.

--- index.js
+++ index.js
@@ -3,8 +3,34 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
 app.get('/', function(req, res) {
-  res.sendFile(__dirname + '/index.html');
+  res.sendFile(__dirname + '/index.html', {
+    headers: {
+      'content-type': 'text/html'
+    }
+  });
 });
+app.get('/style.css', function(req, res) {
+  res.sendFile(__dirname + '/style.css', {
+    headers: {
+      'content-type': 'text/css'
+    }
+  });
+});
+app.get('/alexa-grid.css', function(req, res) {
+  res.sendFile(__dirname + '/alexa-grid.css', {
+    headers: {
+      'content-type': 'text/css'
+    }
+  });
+});
+app.get('/ws_whiteboard.js', function(req, res) {
+  res.sendFile(__dirname + '/ws_whiteboard.js', {
+    headers: {
+      'content-type': 'text/javascript'
+    }
+  });
+});
+
 
 io.on('connection', function(socket){
   socket.on('push', function(msg){
